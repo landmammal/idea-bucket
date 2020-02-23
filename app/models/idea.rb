bad5da89f723e63429ea1214b4c481678ddeffec
landmammal <evolpirate@gmail.com>
2020-02-22 22:47:30 -0500

    adding association between comments and user and ideas

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,4 +2,5 @@ class Idea < ApplicationRecord
   belongs_to :user
   has_many :pledges
   has_many :votes
+  has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :pledges
   has_many :ideas
   has_many :votes
+  has_many :comments
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
